d119e009258016f60d4bfb892135979d1880c144
dmitriiaristov <mitya.aristov@yandex.ru>
2024-12-04 21:31:21 +0200

    modified App.java; add Differ.java; modified build.gradle.kts: add dependencies (picocli); add Makefile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
+
 }
 
 tasks.test {
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,39 @@
+package hexlet.code;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference")
+class Differ implements Callable<Integer> {
+
+//    @Parameters(index = "0", description = "The file whose checksum to calculate.")
+//    private File file;
+
+//    @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
+//    private String algorithm = "SHA-256";
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+//        byte[] fileContents = Files.readAllBytes(file.toPath());
+//        byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
+//        System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
+        return 0;
+    }
+
+    // this example implements Callable, so parsing, error handling and handling user
+    // requests for usage help or version help can be done with one line of code.
+//    public static void main(String... args) {
+//        int exitCode = new CommandLine(new Differ()).execute(args);
+//        System.exit(exitCode);
+//    }
+}
+
