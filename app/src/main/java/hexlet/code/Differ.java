decfa0eedc21395b37441ad6fc9a07a39baa1272
dmitriiaristov <mitya.aristov@yandex.ru>
2024-12-07 18:34:12 +0200

    modified App.java: all supplementary codes transferred to ReadFile.java; modified Differ.java: method call modified; modified build.gradle.kts: add dependency for jackson databind; add ReadFile.java: all method for read and parse files add; add file1.json; add file2.json

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -8,24 +8,32 @@ import picocli.CommandLine.Parameters;
 import java.io.File;
 import java.math.BigInteger;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.MessageDigest;
+import java.util.Map;
 import java.util.concurrent.Callable;
+
+import static hexlet.code.ReadFile.getData;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference")
 class Differ implements Callable<Integer> {
 
     @Parameters(paramLabel = "filepath1", index = "0", description = "path to first file")
-    private File filepath1;
+    private String filepath1;
 
     @Parameters(paramLabel = "filepath2", index = "1", description = "path to second file")
-    private File filepath2;
+    private String filepath2;
 
     @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
     private String format = "stylish";
 
     @Override
     public Integer call() throws Exception { // your business logic goes here...
+        var parsedFile1 = getData(filepath1);
+        var parsedFile2 = getData(filepath2);
+        System.out.println(parsedFile1);
+        System.out.println(parsedFile2);
 //        byte[] fileContents = Files.readAllBytes(file.toPath());
 //        byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
 //        System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
